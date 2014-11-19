0bd4a3513644d46f4c7ae34825bcf67432eb52c9
beck0159 <beck0159@algonquinlive.com>
2014-11-19 16:28:28 -0500

    View Page Modified

---
+++ Car Specs/JsonCalls.cs
@@ -0,0 +1,75 @@
+﻿using Car_Specs.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Diagnostics;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Car_Specs
+{
+
+    class JsonCalls
+    {
+        // create httpClient
+        private HttpClient httpClient;
+
+        public JsonCalls()
+        {
+            // create an instance of httpClient
+            httpClient = new HttpClient();
+            // Limit the max buffer size for the response so we don't get overwhelmed
+            httpClient.MaxResponseContentBufferSize = 256000;
+            httpClient.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)");
+
+        }
+
+        internal async void viewAllCars(String userInput)
+        {
+            try
+            {
+                String responseBodyAsText;
+              
+
+                HttpResponseMessage response = await httpClient.GetAsync("https://api.edmunds.com/api/vehicle/v2/makes?year=" + userInput + "&view=basic&fmt=json&api_key=w7te8pq2racmgdpgp5zxa3b3");
+                response.EnsureSuccessStatusCode();
+                responseBodyAsText = await response.Content.ReadAsStringAsync();
+                responseBodyAsText = responseBodyAsText.Replace("<br>", Environment.NewLine); // Insert new lines
+
+
+                ReturnJObject(responseBodyAsText);
+            }
+            catch (HttpRequestException hre)
+            {
+                //StatusText.Text = hre.ToString();
+            }
+            catch (Exception ex)
+            {
+                // For debugging
+                //StatusText.Text = ex.ToString();
+            }
+        }
+
+        private string ReturnJObject(string responseBodyAsText)
+        {
+ 	       // throw new NotImplementedException();
+
+            return (responseBodyAsText);
+        }
+
+       
+    }
+}
--- Car Specs/View.xaml.cs
+++ Car Specs/View.xaml.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Diagnostics;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Graphics.Display;
@@ -15,6 +17,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 // The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
@@ -27,14 +31,27 @@ namespace Car_Specs
     {
         private NavigationHelper navigationHelper;
         private ObservableDictionary defaultViewModel = new ObservableDictionary();
+        // create httpClient
+        private HttpClient httpClient;
+        // create JSONCALL
+        JsonCalls JCall;
 
         public View()
         {
             this.InitializeComponent();
 
+            // create an instance of httpClient
+            httpClient = new HttpClient();
+            // Limit the max buffer size for the response so we don't get overwhelmed
+            httpClient.MaxResponseContentBufferSize = 256000;
+            httpClient.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)");
+
             this.navigationHelper = new NavigationHelper(this);
             this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
             this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
+
+            // create an instance of JCall
+            JCall = new JsonCalls();
         }
 
         /// <summary>
@@ -126,6 +143,93 @@ namespace Car_Specs
         private void AppBarButton_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(About));
+        }
+
+        private async void searchMake_Click(object sender, RoutedEventArgs e)
+        {
+            // fetch JSON data from hyearInput.Text = "";
+            listView1.Items.Clear();
+            String userInput = yearInput.Text;
+
+            try
+            {
+                String responseBodyAsText;
+                
+                HttpResponseMessage response = await httpClient.GetAsync("https://api.edmunds.com/api/vehicle/v2/makes?year=" + userInput + "&view=basic&fmt=json&api_key=w7te8pq2racmgdpgp5zxa3b3");
+                response.EnsureSuccessStatusCode();
+                responseBodyAsText = await response.Content.ReadAsStringAsync();
+                responseBodyAsText = responseBodyAsText.Replace("<br>", Environment.NewLine); // Insert new lines
+
+                JObject results = JObject.Parse(responseBodyAsText);
+                Debug.WriteLine(results);
+
+
+                // example code http://www.webthingsconsidered.com/2013/08/09/adventures-in-json-parsing-with-c/
+                int i = 0;
+                foreach (var result in results["makes"])
+                {
+
+                    //var type = result["models"];
+                    string makes = (string)result["name"];
+                    // dynamicly make a textblock
+                    TextBlock newTB1 = new TextBlock();
+                    newTB1.Name = makes;
+                    newTB1.Text = makes;
+                    newTB1.FontSize = 20;
+                    // add the newly created TextBlock
+                    listView1.Items.Add(newTB1);
+                    //OutputView.Text += ("Make: " + makes + "\r\n");
+
+                    int a = 0;
+                    foreach (var models in result["models"])
+                    {
+                        var type1 = result["models"][a];
+                        string modelStrings = (string)type1["name"];
+                        var years = type1["years"][0];
+
+                        string modelID = (string)years["id"];
+                        Debug.WriteLine("   Model: " + modelStrings + "\r\n");
+                        // dynamicly make a textblock
+                        TextBlock newTB = new TextBlock();
+                        newTB.Name = modelStrings;
+                        newTB.Text = "  " + modelStrings;
+                        newTB.Tapped += Onb2Click;
+                        newTB.Tag = modelID;
+                        // add the newly created TextBlock
+                        listView1.Items.Add(newTB);
+
+                        a++;
+                    }
+
+                    i++;
+                    Debug.WriteLine("Make: " + makes + "  ");
+                }
+
+            }
+            catch (HttpRequestException hre)
+            {
+                //StatusText.Text = hre.ToString();
+            }
+            catch (Exception ex)
+            {
+                // For debugging
+                //StatusText.Text = ex.ToString();
+            }
+
+        }
+
+        private void Onb2Click(object sender, TappedRoutedEventArgs e)
+        {
+            //throw new NotImplementedException();
+
+            // get specific TextBlock 
+            TextBlock TB = sender as TextBlock;
+            Debug.WriteLine(TB.Tag);
+
+            JCall.viewAllCars((string)TB.Tag);
+
+
+            // Send Tag to models page to use for query
         }
     }
 }
